69e53805b081bf80470d4718d436155945bb17e6
Dino Himaj <dinohimaj@rocketmail.com>
2022-12-24 03:15:01 +0100

    finished Homepage / started doing Revenue Section

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -14,11 +14,11 @@ const Ecommerce = () => {
   return (
     <div className='mt-24'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
-        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lf:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repea bg-cover bg-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
           <div className='flex justify-between items-center'>
             <div>
-              <p>Earnings</p>
-              <p>67,367.56 €</p>
+              <p className='font-bold text-gray-400'>Earnings</p>
+              <p className='text-2xl'>$63,448.78</p>
             </div>
           </div>
           <div className='mt-6'>
@@ -26,7 +26,7 @@ const Ecommerce = () => {
               color='white'
               bgColor='blue'
               text='Download'
-              bordeRadius='10px'
+              borderRadius='10px'
               size='md'
             />
           </div>
@@ -44,8 +44,50 @@ const Ecommerce = () => {
               >
                 {item.icon}
               </button>
+              <p className='mt-3'>
+                <span className='text-lg font-semibold'>{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className='text-sm text-gray-400  mt-1'>{item.title}</p>
             </div>
           ))}
+        </div>
+      </div>
+      <div className='flex gap-10 flex-wrap justify-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+          <div className='flex justify-between'>
+            <p className='font-semibold text-xl'>Revenue Updates</p>
+            <div className='flex items-center gap-4'>
+              <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+
+              <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+
+          <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+            <div className='border-r-1 m-4 pr-10'>
+              <div>
+                <p>
+                  <span className='text-3xl font-semibold'>60,768 €</span>
+                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>
+                    23%
+                  </span>
+                </p>
+              </div>{' '}
+            </div>
+          </div>
         </div>
       </div>
     </div>
